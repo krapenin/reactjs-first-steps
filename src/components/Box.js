e99b19c1a07f98c59d21e12eb90716b3c4c7f886
Marcos Vilches <mvilches.zajara@gmail.com>
2021-11-11 20:26:10 +0100

    Interaccionando entre componentes

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,13 +3,13 @@ import Title from "./TItle";
 import TextBox from "./TextBox";
 import Button from "./Button";
 
-export default function Box({data: {classValue, title, textName, buttonValue}, callBack}) {
+export default function Box({data: {classValue, title, textName, buttonValue, buttonDisabled}, callBack}) {
 
     return (
         <div className={classValue}>
             <Title text={title}/>
             <TextBox name={textName}/>
-            <Button value={buttonValue}  callBack={callBack}/>
+            <Button value={buttonValue} disabled={buttonDisabled} callBack={callBack}/>
         </div>
     )
 }
