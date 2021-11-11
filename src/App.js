a59e8ee104baffd0907691d6f47d67dbbab257f8
Marcos Vilches <mvilches.zajara@gmail.com>
2021-11-11 20:26:10 +0100

    Interaccionando entre componentes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import './App.css';
 import Box from "./components/Box";
+import {useState} from "react";
 
 
 function App() {
 
-    const handleOnClickButton = () => {
-        console.info("Acabas de pulsar el check status 1");
-    };
-
-    const handleOnClickButton2 = () => {
-        console.info("Acabas de pulsar el check status 2");
-    };
-
-    const propsCheckStatusBox = {
+    const [propsCheckStatusBox, setPropsCheckStatusBox] = useState({
         "classValue": "box-class",
         "title": "Check status",
         "textName": "textBox1",
         "buttonValue": "Check",
-    }
+        "buttonDisabled": ""
+    })
 
-    const propsCheckStatusBox2 = {
+    const [propsCheckStatusBox2, setPropsCheckStatusBox2] = useState({
         "classValue": "box-class",
         "title": "Check status 2",
         "textName": "textBox2",
-        "buttonValue": "Check 2"
+        "buttonValue": "Check 2",
+        "buttonDisabled": ""
+    })
+
+    const toggleButton = (callBack) => {
+        callBack( prevState => ({
+            ...prevState,
+            "buttonDisabled": prevState.buttonDisabled === "disabled" ? "" : "disabled"
+        }));
+
     }
 
+    const handleOnClickButton = () => {
+        toggleButton(setPropsCheckStatusBox2);
+        setTimeout(function() {
+            toggleButton(setPropsCheckStatusBox2)
+        }, 2000 )
+    };
+
+    const handleOnClickButton2 = () => {
+        toggleButton(setPropsCheckStatusBox);
+        setTimeout(function() {
+            toggleButton(setPropsCheckStatusBox)
+        }, 2000 )
+    };
 
   return (
     <div className="App">
@@ -33,6 +49,6 @@ function App() {
         <Box data={propsCheckStatusBox2} callBack={handleOnClickButton2}/>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function Button({value, callBack}) {
+export default function Button({value, disabled, callBack}) {
     return (
-        <><input type="button" value={value} onClick={callBack}/></>
+        <><input type="button" disabled={disabled}  value={value} onClick={callBack}/></>
     );
 }
